44671b462cbcaa73472e917a079f78ca06d1bc90
m-koto <qd.peace.maker@gmail.com>
2022-08-07 23:18:32 +0900

    create_dotenv

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const userRoute = require("./routes/users.js")
 const authRoute = require("./routes/auth.js")
 const postRoute = require("./routes/posts.js")
 const PORT = 3000;
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose.connect(process.env.MONGOURL).then(() => {
+  console.log("DBと接続中・・・")
+}).catch((err) => {
+  console.log(err)
+});
 
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
