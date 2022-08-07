a461198c34cd369182cc9e8bf7261087c51c876c
m-koto <qd.peace.maker@gmail.com>
2022-08-07 22:25:52 +0900

    create_route

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
+const userRoute = require("./routes/users.js")
+const authRoute = require("./routes/auth.js")
+const postRoute = require("./routes/posts.js")
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send("hello express");
-});
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
 
 app.listen(PORT, () => console.log("サーバーが起動しました"));
