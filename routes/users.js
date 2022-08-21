93ce410366914a898253330fc0ef29862ea6ed0e
m-koto <qd.peace.maker@gmail.com>
2022-08-22 01:08:51 +0900

    create_userInfoUpdate

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,27 @@
 const router = require("express").Router();
+const User = require("../models/User");
+
+// CRUD
+// ユーザー情報の更新
+router.put("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try{
+      const user = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      });
+      // 204ではなく200を返すようにしている
+      res.status(200).json("ユーザー情報が更新されました");
+    } catch(err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json("編集権限がありません");
+  }
+});
+
+// ユーザー情報の削除
+// ユーザー情報の取得
+
 
 // テスト用callback関数
 router.get("/", (req, res) => {
